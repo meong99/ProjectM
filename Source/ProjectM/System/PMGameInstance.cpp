cb823b6e0d8da381f78a068009e72e1abd29c269
Mchae_In_Mantisco <mchae.works@gmail.com>
2024-12-02 11:49:44 +0900

    GameInstance 및 AssetManager 추가 및 엔진에 등록

---
+++ Source/ProjectM/System/PMGameInstance.cpp
@@ -0,0 +1,5 @@
+#include "PMGameInstance.h"
+
+UPMGameInstance::UPMGameInstance()
+{
+}
---
+++ Source/ProjectM/System/PMGameInstance.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "PMGameInstance.generated.h"
+
+UCLASS()
+class PROJECTM_API UPMGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+/*
+* Engine Functions
+*/
+public:
+	UPMGameInstance();
+
+/*
+* Member Functions
+*/
+public:
+
+/*
+* Member Variables
+*/
+public:
+};
